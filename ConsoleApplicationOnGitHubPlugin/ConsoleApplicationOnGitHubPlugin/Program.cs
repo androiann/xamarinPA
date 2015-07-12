40a9daefabb0be2abee993fe131fdad13e6a9100
androiann <androiann@gmail.com>
2015-07-12 14:38:56 +0100

    hello github world

---
+++ ConsoleApplicationOnGitHubPlugin/ConsoleApplicationOnGitHubPlugin/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationOnGitHubPlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub World");
+            Console.ReadLine();
+
+        }
+    }
+}
